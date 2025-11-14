8e1a02d22be7fdae95b6d945161c8f9468ba3da4
Shuaib Muhammad <FlySlips@gmail.com>
2025-11-14 11:43:46 -0800

    More updates

--- selects.sql
+++ selects.sql
@@ -9,13 +9,14 @@ select attributes->>'WaferId' from products;
 --	INNER JOIN measurements_4 AS m4 ON m2.context_id = m4.context_id
 --	WHERE m1.context_id = 1
 
-select conxt.id as cnxt_id, prod.id as prod_id, prod.attributes, s.attributes from contexts AS conxt
+select m1.col_1, conxt.id cnxt_id, prod.id prod_id, prod.attributes Product, s.attributes Source from contexts AS conxt
 	INNER JOIN products AS prod ON conxt.product_id = prod.id
-	INNER JOIN sources AS s ON conxt.source_id = s.id
-	WHERE prod.attributes->>'WaferId' LIKE 'Wafer_%'
-		  AND prod.attributes->>'Family' LIKE 'I%'
-		  AND prod.attributes->>'LotId' = 'Lot_ABC'
-		  AND s.attributes->>'Chamber' = '1B'
+	INNER JOIN sources AS s ON conxt.source_id = s.id	
+	INNER JOIN measurements_1 m1 ON conxt.id = m1.context_id
+	WHERE prod.attributes->>'Lot' = 'Lot_1'
+		  AND prod.attributes->>'Slot' = 'L1.S2'
+		  AND prod.attributes->>'Wafer' = 'L1.S2.W1'
+		  AND s.attributes->>'Chamber' = 'F1.A1.T2.C2'
 
 DO $$
 DECLARE 
@@ -84,12 +85,27 @@ DO $$
 DECLARE 
 mId BOOLEAN;
 Begin
-	CALL add_measurements(3, '{1.1, 2.1, 3.1, 4.1, 5.1}'::REAL[], mId);
+	CALL add_measurements(7, '{1.1, 2.1, 3.1, 4.1, 5.1}'::REAL[], mId);
 	RAISE INFO  'collection_plan_id: %', mId;					 
 END $$;
 SELECT * FROM measurements_1;
 SELECT * FROM measurements_2;
 SELECT * FROM measurements_4;
+SELECT * FROM collection_plans WHERE cp_name = 'Collection_Plan_3';
 
+
+SELECT * 
+FROM measurements_1 m1
+INNER JOIN measurement_2 m2 ON m1.context_id = m2.context_id
+
+
+SELECT * FROM get_svid_column('CP5.T4.C9', 'Collection_Plan_5');
+
+SELECT * FROM get_measurement('CP7.T2.C1', 'Collection_Plan_9', '{"source":{"Chamber": "F1.A1.T2.C2"}, "product":{"Wafer": "L1.S2.W1"}}');
+
+SELECT * FROM collection_plans;
+INSERT INTO collection_plans (svids, cp_name, description)
+VALUES('{"table_1": {"col_1": "CP7.T2.C1"},"table_2": {"col_1": "CP7.T2.C1", "col_2": "CP7.T2.C2"}}',
+	   'Collection_Plan_7', 'Collection_Plans: CP7.T1.T2.T4 Description');
 
 
